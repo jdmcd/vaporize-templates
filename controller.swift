92bb21faa273e4512d4c05f4510774111bf10cf4
Jimmy School <mcdappdev@gmail.com>
2017-04-03 22:09:52 -0500

    controller file

---
+++ controller.swift
@@ -0,0 +1,14 @@
+import Vapor
+import HTTP
+
+final class VAR_CONTROLLER_NAME {
+    let drop: Droplet
+
+    init(drop: Droplet) {
+        self.drop = drop
+    }
+
+    func addRoutes() {
+
+    }
+}
